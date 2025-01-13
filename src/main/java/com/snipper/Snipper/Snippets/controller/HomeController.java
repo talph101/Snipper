9a775f29ab67f100531bf3ca03cc480a2cb0751f
Tiffany <139176913+talph101@users.noreply.github.com>
2025-01-12 18:11:34 -0600

    implementing Auth0

---
+++ src/main/java/com/snipper/Snipper/Snippets/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.snipper.Snipper.Snippets.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
+    }
+}
